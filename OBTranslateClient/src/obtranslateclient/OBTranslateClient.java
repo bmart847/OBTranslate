9bcd175c96fb8f3eb63dd19fb4139c9e7b61be0b
Bryan Martin <bryan.martin@mymail.champlain.edu>
2016-04-07 14:46:43 -0400

    Set up project structures

---
+++ OBTranslateClient/src/obtranslateclient/OBTranslateClient.java
@@ -0,0 +1,28 @@
+/*
+Author: Bryan Martin and Owen Enders
+Class: CSI-235-01
+Assignment: Final Project - OBTranslate
+Due Date: 04/24/2016
+Description:
+    The OBTranslateClient allows the users to communicate with the OBTranslate
+Server. Using the client the user can translate text to different languages.
+
+Certification of Authenticity:
+    I certify that this is entirely my own work, except where I have given fullydocumented
+    references to the work of others. I understand the definition and
+    consequences of plagiarism and acknowledge that the assessor of this assignment
+    may, for the purpose of assessing this assignment:
+    - Reproduce this assignment and provide a copy to another member of
+    academic staff; and/or
+    - Communicate a copy of this assignment to a plagiarism checking service
+    (which may then retain a copy of this assignment on its database for
+    the purpose of future plagiarism checking)
+*/
+package obtranslateclient;
+
+public class OBTranslateClient {
+    public static void main(String[] args) {
+        // Client Main Code Goes Here
+    }
+    
+}
---
+++ OBTranslateServer/src/obtranslateserver/OBTranslateServer.java
@@ -0,0 +1,30 @@
+/*
+Author: Bryan Martin and Owen Enders
+Class: CSI-235-01
+Assignment: Final Project - OBTranslate
+Due Date: 04/24/2016
+Description:
+    The OBTranslateServer acts as a relay between the users of OBTranslateClient
+and Yandex (the translation service we are using). The server recieves the text
+that the user desires to translate and then responds to the user with the
+translated text.
+
+Certification of Authenticity:
+    I certify that this is entirely my own work, except where I have given fullydocumented
+    references to the work of others. I understand the definition and
+    consequences of plagiarism and acknowledge that the assessor of this assignment
+    may, for the purpose of assessing this assignment:
+    - Reproduce this assignment and provide a copy to another member of
+    academic staff; and/or
+    - Communicate a copy of this assignment to a plagiarism checking service
+    (which may then retain a copy of this assignment on its database for
+    the purpose of future plagiarism checking)
+*/
+package obtranslateserver;
+
+public class OBTranslateServer {
+    public static void main(String[] args) {
+        //Server Main Code Goes Here
+    }
+    
+}
